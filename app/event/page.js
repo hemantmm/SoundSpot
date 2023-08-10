3e34bb503aa7a85a8a540b4df3e5b941189dc910
Chirag <56735482+Chirag018@users.noreply.github.com>
2023-08-10 14:11:31 +0530

    changes

---
+++ app/event/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const event = () => {
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+};
+
+export default event;
---
+++ app/subscri/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subscription = () => {
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+};
+
+export default Subscription;
